ab1042083ff257df79bf58ad515c1a1815bcfb8c
Merijn Tieltjes <merijn.tieltjes@nedap.com>
2018-10-17 09:30:33 +0200

    Added Atom packages install script

---
+++ install-atom-packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Formatting
+apm install atom-beautify prettier-atom
+
+# Utils
+apm install duplicate-line-or-selection file-icons highlight-selected sublime-style-column-selection tab-foldername-index
+
+# HTML/JS/React
+apm install atom-ternjs atom-wrap-in-tag autoclose-html autocomplete-modules color-picker docblockr js-hyperclick hyperclick pigments linter-eslint language-babel
